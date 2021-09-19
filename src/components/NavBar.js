1c0c12ca6c209599359ee8f100bf1fec6c78aa0d
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-19 19:09:45 +0530

    Version I: Completed with sample output

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,15 +8,16 @@ export class NavBar extends Component {
                     <div className="container-fluid">
                         <img src="favicon.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top" />
                         <a className="navbar-brand" href="/">NEWSTROM</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
                             </ul>
+
                         </div>
                     </div>
                 </nav>
