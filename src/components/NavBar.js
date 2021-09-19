5ffb3c4227e01d7ceac9d12700b60446ee22b612
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-19 16:35:23 +0530

    Changed Component struture

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <img src="favicon.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top" />
+                        <a className="navbar-brand" href="/">NEWSTROM</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavBar
